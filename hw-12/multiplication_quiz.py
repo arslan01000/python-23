f57b4fb5921b2538003ee1b13aa8ebdc82dff1d4
Arslan <127810063+arslan01000@users.noreply.github.com>
2025-02-08 23:56:36 -0600

    Update and rename task-2.py to multiplication_quiz.py

--- hw-12/multiplication_quiz.py
+++ hw-12/multiplication_quiz.py
@@ -1,3 +1,5 @@
+# A multiplication quiz that tracks the user's accuracy percentage.
+
 import random
 
 statistic_list = [True, False, False, False, True, False]
@@ -5,7 +7,7 @@ statistic_list = [True, False, False, False, True, False]
 num1 = random.randint(1, 9)
 num2 = random.randint(1, 9)
 
-user_answer = int(input(f"Сколько будет {num1} * {num2}? \n"))
+user_answer = int(input(f"How much is {num1} * {num2}? \n"))
 
 correct_answer = num1 * num2
 
@@ -21,6 +23,6 @@ incorrect_count = statistic_list.count(False)
 total_count = len(statistic_list)
 percentage = (correct_count / total_count) * 100
 
-print(f"{'Правильных ответов:':<35}{correct_count}")
-print(f"{'Неправильных ответов:':<35}{incorrect_count}")
-print(f"{'Процент правильных ответов:':<35}{percentage:.2f}%")
+print(f"{'Correct answers:':<35}{correct_count}")
+print(f"{'Incorrect answers:':<35}{incorrect_count}")
+print(f"{'Percentage of correct answers:':<35}{percentage:.2f}%")
