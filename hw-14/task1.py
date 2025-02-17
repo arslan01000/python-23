1e0a381664b448c6b1aa598cd071611a201636c9
Arslan <127810063+arslan01000@users.noreply.github.com>
2025-02-17 16:02:39 -0600

    Create task1.py

---
+++ hw-14/task1.py
@@ -0,0 +1,11 @@
+a = float(input("Введите первое число: "))
+b = float(input("Введите второе число: "))
+
+max_num = max(a, b)
+min_num = min(a, b)
+median = (a + b)/ 2
+
+print(f"Max = {max_num}")
+print(f"Median = {median}")
+print(f"Min = {min_num}")
+
