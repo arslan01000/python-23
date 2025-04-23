2a8214fccae954dd30ea6959ef186d2d2148bf7b
Arslan <127810063+arslan01000@users.noreply.github.com>
2025-04-22 23:57:17 -0500

    Update task-4.py

--- hw-10/task-4.py
+++ hw-10/task-4.py
@@ -6,3 +6,4 @@ jack_score = int(input("Введите оценку для Джека: \n"))
 jane_score = int(input("Введите оценку Джейн: \n"))
 result = math.ceil((jake_score + john_score + jack_score + jane_score) / 4)
 print("Среднее по оценкам:", result)
+
